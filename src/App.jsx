e43d5c8602e6af50c1f2fd4bd4cc0c0d2f61ab41
Piyush Verma <piyushvermaghga@gmail.com>
2025-07-15 17:36:44 +0530

    Remove some part

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Experiences from "./components/Experiences";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
